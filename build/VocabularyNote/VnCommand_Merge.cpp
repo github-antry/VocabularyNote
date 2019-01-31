50ee048691c481647c9bccc6e04bfc4e029a420d
github-antry <weianquan1989@sina.com>
2019-02-01 00:16:37 +0800

    none

--- build/VocabularyNote/VnCommand_Merge.cpp
+++ build/VocabularyNote/VnCommand_Merge.cpp
@@ -22,17 +22,163 @@ CVnMerge::CVnMerge(void)
 
 CVnMerge::~CVnMerge(void)
 {
+	// LIST 内存释放
 }
+
+
+std::wstring string2wstring(std::string str)  
+{  
+	std::wstring result;  
+	//获取缓冲区大小，并申请空间，缓冲区大小按字符计算  
+	int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), NULL, 0);  
+	wchar_t* buffer = new wchar_t[len + 1];  
+	//多字节编码转换成宽字节编码  
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), buffer, len);  
+	buffer[len] = '\0';             //添加字符串结尾  
+	//删除缓冲区并返回值  
+	result.append(buffer);  
+	delete[] buffer;  
+	return result;  
+}  
+
+//将wstring转换成string  
+std::string wstring2string(std::wstring wstr)  
+{  
+	std::string result;  
+	//获取缓冲区大小，并申请空间，缓冲区大小事按字节计算的  
+	int len = WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), wstr.size(), NULL, 0, NULL, NULL);  
+	char* buffer = new char[len + 1];  
+	//宽字节编码转换成多字节编码  
+	WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), wstr.size(), buffer, len, NULL, NULL);  
+	buffer[len] = '\0';  
+	//删除缓冲区并返回值  
+	result.append(buffer);  
+	delete[] buffer;  
+	return result;  
+}
+
+string UTF82WCS(const char* szU8)
+{
+	//预转换，得到所需空间的大小;
+	int wcsLen = ::MultiByteToWideChar(CP_UTF8, NULL, szU8, strlen(szU8), NULL, 0);
+
+	//分配空间要给'\0'留个空间，MultiByteToWideChar不会给'\0'空间
+	wchar_t* wszString = new wchar_t[wcsLen + 1];
+
+	//转换
+	::MultiByteToWideChar(CP_UTF8, NULL, szU8, strlen(szU8), wszString, wcsLen);
+
+	//最后加上'\0'
+	wszString[wcsLen] = '\0';
+
+	wstring wtmp = wszString;
+
+	delete[] wszString;
+	wszString = NULL;
+
+	return wstring2string(wtmp);
+}
+
 
 void CVnMerge::Run(const struct _INSTRUCTIONS_ &ins)
 {
-	string file = "E:\\W009_GitHub\\GanttSchedule\\bookmarks_google - 副本.html";
+	string file = "E:\\W009_GitHub\\GanttSchedule\\bookmarks_google.html";
 
-	string html = readFileIntoString(file.c_str());
-	//string html = UTF8ToGBK(tmp.c_str());
+	string tmp = readFileIntoString(file.c_str());
+	string html = UTF82WCS(tmp.c_str());
 
 	HTML::ParserDom parser;
 	tree<HTML::Node> dom = parser.parseTree(html);
+
+	tree<HTML::Node>::pre_order_iterator it = dom.begin();
+	tree<HTML::Node>::pre_order_iterator end = dom.end();
+
+	//书签栏	
+	bool bRootTurned = false;
+	CVnClassification *pH3Root = new CVnClassification;
+	m_lstClassification.push_back(pH3Root);
+
+	CVnClassification *pNewClassification = nullptr;
+	CVnItem *pNewItem = nullptr;
+
+	bool bLastNewIsClass = false;
+	for (; it != end; ++it)
+	{
+		it->parseAttributes();
+
+		string tmpText = it->text();
+		string tmpTagName = it->tagName();
+
+		if (strcmp(tmpTagName.c_str(), "H3") == 0 )
+		{
+			if(it->attribute("folder").first)
+			{	
+				pH3Root->m_add_date = it->attribute("date").second;
+				pH3Root->m_last_modified = it->attribute("modified").second;
+				pH3Root->m_personal_toolbar_folder = it->attribute("folder").second;
+
+				bRootTurned = true;
+			}
+			else
+			{				
+				pNewClassification = new CVnClassification;
+				m_lstClassification.push_back(pNewClassification);
+
+				pNewClassification->m_add_date = it->attribute("date").second;
+				pNewClassification->m_last_modified = it->attribute("modified").second;
+
+				bLastNewIsClass = true;
+			}
+		}
+		else if (strcmp(tmpTagName.c_str(), "A") == 0 )
+		{
+			pNewItem = new CVnItem;
+			pNewItem->m_href = it->attribute("href").second;
+			pNewItem->m_add_date = it->attribute("date").second;
+
+			if(pNewClassification)
+			{
+				pNewClassification->AddNode(pNewItem);
+			}
+			else
+			{
+				pH3Root->AddNode(pNewItem);
+			}			
+
+			bLastNewIsClass = false;
+		}
+		else if(!it->isTag() && !it->isComment())
+		{
+			if(pNewItem == nullptr && pNewClassification == nullptr)
+			{
+				if(bRootTurned && pH3Root->m_label.empty() && pH3Root->m_label.size() == 0)
+				{
+					pH3Root->m_label = tmpText;
+				}
+			}
+			else
+			{
+				if(!bLastNewIsClass)
+				{
+					if(pNewItem->m_label.empty())
+					{
+						pNewItem->m_label = tmpText;
+					}
+				}
+				else
+				{
+					if(pNewClassification->m_label.empty())
+					{
+						pNewClassification->m_label = tmpText;
+					}
+				}
+			}
+		}
+		else
+		{
+
+		}
+	}
 
 	std::cout << dom << endl;
 }
@@ -139,4 +285,41 @@ void CVnMerge::WriteToHtml_DL_DEGIN(ofstream &result)
 void CVnMerge::WriteToHtml_DL_END(ofstream &result)
 {
 	result << "</DL><p>"<<endl;
+}
+
+
+//////////////////////////////////////////////////////////////
+//                 CVnItem
+//////////////////////////////////////////////////////////////
+
+CVnItem::CVnItem()
+{
+}
+
+CVnItem::~CVnItem()
+{
+}
+
+
+//////////////////////////////////////////////////////////////
+//                CVnClassification
+//////////////////////////////////////////////////////////////
+
+CVnClassification::CVnClassification()
+{
+}
+
+CVnClassification::~CVnClassification()
+{
+	// LIST 内存释放
+}
+
+std::list<CVnItem *> CVnClassification::GetChildren()
+{
+	return m_lstChild;
+}
+
+void CVnClassification::AddNode(CVnItem *_node)
+{
+	m_lstChild.push_back(_node);
 }
--- build/VocabularyNote/VnCommand_Merge.h
+++ build/VocabularyNote/VnCommand_Merge.h
@@ -11,31 +11,66 @@ author      : andy
 #include "VnCommand.h"
 #include "VnCommonDef.h"
 
+class CVnItem
+{
+public:
+	CVnItem();
+	~CVnItem();
+
+public:
+	std::string m_add_date;
+	std::string m_href;
+	std::string m_label;
+};
+
+class CVnClassification
+{
+public:
+	CVnClassification();
+	~CVnClassification();
+
+public:
+	std::list<CVnItem *> GetChildren();
+	void AddNode(CVnItem *_node);
+
+public:
+	std::string m_add_date;
+	std::string m_last_modified;
+	std::string m_personal_toolbar_folder;
+
+	std::string m_label;
+
+private:
+	std::list<CVnItem *> m_lstChild;
+};
+
 class CVnMerge : public CVnCommand
 {
 public:
 	CVnMerge(void);
 	virtual ~CVnMerge(void);
 
-	virtual void Run(const struct _INSTRUCTIONS_ &ins) override; 
+	virtual void Run(const struct _INSTRUCTIONS_ &ins) override;
 
 protected:
 	//从文件读入到string里
-	string CVnMerge::readFileIntoString(const char * filename);
+	string CVnMerge::readFileIntoString(const char *filename);
 
 	void WriteToHtml();
 	void WriteToHtml_Header(ofstream &result);
 	void WriteToHtml_DT_H3(ofstream &result, long add_date,
-		long last_modified,
-		bool toolbar_folder,
-		string text);
+												 long last_modified,
+												 bool toolbar_folder,
+												 string text);
 	void WriteToHtml_DT_A(ofstream &result,
-		string href,
-		long date,
-		string text);
+												string href,
+												long date,
+												string text);
 	void WriteToHtml_DL_DEGIN(ofstream &result);
 	void WriteToHtml_DL_END(ofstream &result);
+
+private:
+	std::list<CVnClassification *> m_lstClassification;
 };
 
 #endif
-
